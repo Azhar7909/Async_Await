1ad47745a3f044078a4b0ffe693a61e3578846a1
Muhammad Azhar Hussain <44233088+Azhar7909@users.noreply.github.com>
2020-12-26 18:26:12 +0500

    updates

--- index.js
+++ index.js
@@ -1,84 +1,33 @@
-// Syncronous And Asyncronous
-console.log("Hello world");
+console.log("Fetch Api");
 
-setTimeout(function() {
-    console.log("this message will be display late");
-},5000);
+// const api = fetch("https://jsonplaceholder.typicode.com/posts/1")
+// .then(function(res) {
+//     console.log(res, "res");
+// })
+// console.log(api,"api");
 
-console.log("Soon");
-
-
-// CallBack Function
-// Simple
-function AlertGreeting(message, GreetingFunction) {
-    GreetingFunction(message)
+async function getData() {
+  const api = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  console.log(api, "api");
+  const json = await api.json();
+  console.log(json, "json");
 }
+// getData();
 
-function GreetingFunction(mes) {
-    alert(mes)
+async function postApi() {
+  const api = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "Smart",
+      body: "Wellcome",
+      userId: 1,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  });
+  console.log(api, "api");
+  const json = await api.json();
+  console.log(json, "json");
 }
-
-setTimeout(function() {
-    AlertGreeting("Hello Dear",GreetingFunction)
-},5000)
-// Complex
-function AlertGreetingFunction(msg,GreetingFunction) {
-    if (msg === "Good Morning") {
-        GreetingFunction(msg)
-    } else if (msg === "Good AfterNoone") {
-        GreetingFunction(msg)
-    } else if (msg === "Good Evening") {
-        GreetingFunction(msg)
-    } else{
-        GreetingFunction(msg)
-    }
-}
-AlertGreetingFunction("Good Night",GreetingFunction)
-
-
-
-// Promises
-// Simple
-let promise = new Promise ( function (res , rej) {
-    console.log("New Promise created");
-    res()
-})
-promise.then(function() {
-    console.log("Promise resolve");
-})
-// Detail
-function Success () {
-    console.log("Success");
-}
-function Failure() {
-    console.log("Failed");
-}
-
-function CheckUserInput(userInput) {
-    new Promise ( function (res, rej) {
-        if (userInput) {
-            Success()
-            res()
-        } else {
-            Failure()
-            rej()
-        }
-    })
-}
-CheckUserInput(false);
-// More Details
-const anyFunction = () =>{
-    return Math.floor(Math.random() * 10) % 2 === 0? true : false ;
-}
-const resolveFunction = () =>{
-    console.log("resolveFunction Run Successfully")
-}
-const rejectFunction = () =>{
-    console.log("rejectFunction Run Successfully")
-}
-
-const result = new Promise((res, rej) => {
-    setTimeout( () => (anyFunction()? res() : rej()),2000);
-})
-result.then(resolveFunction).catch(rejectFunction);
-console.log("Hello world");
+postApi()
